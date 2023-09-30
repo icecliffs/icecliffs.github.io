e99108717e81725ba68c54710953d70e9c757d1e
IceCliffs <security@icecliffs.cn>
2023-09-30 23:34:58 +0800

    upload

---
+++ blog/index.js
@@ -0,0 +1,46 @@
+function cnC()
+{
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{
+		//  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{
+		// IE6, IE5 浏览器执行代码
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			document.getElementById("langContent").innerHTML=xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET","http://127.0.0.1:5500/zh.txt",true);
+	xmlhttp.send();
+}
+function enE()
+{
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{
+		//  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{
+		// IE6, IE5 浏览器执行代码
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			document.getElementById("langContent").innerHTML=xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET","http://127.0.0.1:5500/en.txt",true);
+	xmlhttp.send();
+}
---
+++ status/js/config.js
@@ -0,0 +1,7 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+'u889001-b7ccda4a9aa1bbe2dd08c62c'
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
